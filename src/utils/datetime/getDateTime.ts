4f8a7901f3ae799c47d26df0046e766133b6b3a7
Antonio-Savio <antonio.savio28@gmail.com>
2025-03-25 19:32:47 -0300

    fix: specifying date and time options

--- src/utils/datetime/getDateTime.ts
+++ src/utils/datetime/getDateTime.ts
@@ -1,17 +1,25 @@
-export function getTime(dateTime: Date){
+export function getTime(dateTime: Date | string){
     const date = new Date(dateTime)
 
-    const time = date.toLocaleTimeString().slice(0, 5);
+    const time = date.toLocaleTimeString("pt-BR", {
+        timeZone: 'America/Sao_Paulo',
+        hour: '2-digit',
+        minute: '2-digit'
+    })
 
     return time;
 }
 
-export function getDate(dateTime: Date){
+export function getDate(dateTime: Date | string){
     const date = new Date(dateTime)
 
-    const calendar = date.toLocaleDateString();
+    const dayAndDate = date.toLocaleDateString('pt-BR', { 
+        weekday: 'short',
+        timeZone: 'America/Sao_Paulo',
+        day: '2-digit',
+        month: '2-digit',
+        year: '2-digit'
+    })
 
-    const dayOfWeek = date.toLocaleDateString('pt-BR', { weekday: 'short' })
-
-    return `${dayOfWeek} ${calendar}`;
+    return dayAndDate;
 }
