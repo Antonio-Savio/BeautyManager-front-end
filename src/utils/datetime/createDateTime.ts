726bd70340fed6c791198cf4b2854b5dc42ac0c4
Antonio-Savio <antonio.savio28@gmail.com>
2025-03-25 05:22:42 -0300

    fix: updating to utc format

--- src/utils/datetime/createDateTime.ts
+++ src/utils/datetime/createDateTime.ts
@@ -3,9 +3,7 @@ import moment from 'moment-timezone'
 export function createDateTime(date: string, time: string): string {
     const dateTimeString = `${date} ${time}`;
 
-    const dateTime = moment.tz(dateTimeString, 'YYYY-MM-DD HH:mm', 'America/Sao_Paulo');
+    const dateTime = moment.tz(dateTimeString, 'America/Sao_Paulo');
 
-    const isoDate = dateTime.format('YYYY-MM-DDTHH:mm:ss.SSSZ');
-
-    return isoDate;
+    return dateTime.utc().format('YYYY-MM-DDTHH:mm:ss.SSS[Z]');
 }
